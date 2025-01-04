4a4bb4322cbb98a9f589f7e60d0732513a61a9fe
luis miguel <luismiguelbrancalion2@gmail.com>
2025-01-04 20:29:04 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const book = document.getElementById('biblebooks')
+const chapter = document.getElementById('chapters')
+const verse = document.getElementById('verse')
+const versediv = document.getElementById('versediv')
+const btnsearch = document.getElementById('send')
+const btnzoom = document.getElementById('zoom')
+const btnimg = document.querySelector('img')
+let clickvar=0
+btnsearch.addEventListener('click',()=>{
+    versediv.innerHTML = ''
+    if (book.value == '' || chapter.value == ''){
+     alert('por favor,digite o livro e capitulo que deseja ler')
+    }else if(book.value != '' && chapter.value != '' && verse.value == ''){ 
+        document.getElementById('errorp').setAttribute('hidden',1)
+       
+        fetch(`https://bible-api.com/${book.value} ${chapter.value}?translation=almeida`)
+        .then(resp => {
+            return resp.json()
+        })
+        .then(data =>{
+          data.verses.forEach(verse => {
+            const versep = document.createElement('div')
+            versep.id ='versep' 
+            versep.innerHTML = `${verse.verse}   ${verse.text}`
+  
+           versediv.appendChild(versep)
+          });
+        }) 
+        .catch(error => {
+            document.getElementById('errorp').removeAttribute('hidden')
+          });
+
+    }else if(book.value != '' && chapter.value != '' && verse.value != ''){
+        document.getElementById('errorp').setAttribute('hidden',1)
+
+        fetch(`https://bible-api.com/${book.value} ${chapter.value}:${verse.value}?translation=almeida`)
+        .then(resp =>{
+            return resp.json()
+        }).then(data=>{
+
+            data.verses.forEach(verse => {
+                const versep = document.createElement('div')
+                versep.id ='versep' 
+                versep.innerHTML = `${verse.verse}   ${verse.text}`
+      
+               versediv.appendChild(versep)
+            })
+            
+        })
+        .catch(error => {
+            document.getElementById('errorp').removeAttribute('hidden')
+          });
+
+    }
+})
+btnzoom.addEventListener('click',()=>{
+    clickvar ++
+    if(clickvar >1 ){
+     clickvar = 0
+    }
+    if (clickvar == 0){
+        versediv.style.fontSize = 30 +'px'
+
+    }else if(clickvar == 1){
+        versediv.style.fontSize ='medium'
+    }
+})
+
