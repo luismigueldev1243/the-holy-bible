3730ecfce64e1ba731758815bf1eeb4cad456d9c
luis miguel <luismiguelbrancalion2@gmail.com>
2025-01-24 10:45:52 -0300

    adicionei novas funcoes relacionadas a local storage e responsividade

--- script.js
+++ script.js
@@ -4,13 +4,93 @@ const verse = document.getElementById('verse')
 const versediv = document.getElementById('versediv')
 const btnsearch = document.getElementById('send')
 const btnzoom = document.getElementById('zoom')
+
+const otherOPbtn = document.getElementById('other-options')
+const otherOPdialog = document.getElementById('other-op-dialog')
+const closeDIALOGbtn = document.getElementById('close-btn')
+
+const bcBTN = document.getElementById('background-color-btn')
+
+const helpBTN = document.getElementById('help-btn')
+const helpDialog = document.getElementById('help-dialog')
+const closeHELPdialog = document.getElementById('close-help-dialog')
+
+const direitos = document.getElementById('direitos')
+
 let clickvar=0
+let themevar=0
+document.body.style.backgroundColor = localStorage.getItem('theme') || 'white' 
+let lcgetted = JSON.parse(localStorage.getItem('chapEbook')) || "none"
+if(lcgetted != 'none'){
+    book.value = lcgetted['book']
+    chapter.value = lcgetted['chapter']
+    alert('seu progresso ficou salvo desde quando voce saiu')
+}else{
+    
+}
+
+
+helpBTN.addEventListener('click',()=>{
+    helpDialog.showModal()
+    
+})
+closeHELPdialog.addEventListener('click',()=>{
+    helpDialog.close()
+})
+
+
+otherOPbtn.addEventListener('click',()=>{
+     otherOPdialog.showModal()
+})
+closeDIALOGbtn.addEventListener('click',()=>{
+    otherOPdialog.close()
+})
+bcBTN.addEventListener('click',()=>{    
+    themevar++
+    if(themevar == 0){
+        localStorage.setItem('theme','white')
+        document.body.style.backgroundColor = 'white'
+        bcBTN.innerHTML = '<i class="fa-solid fa-arrow-down"></i> branco'
+
+        direitos.style.color = 'rgb(135, 135, 135)'
+
+    }else if(themevar == 1){
+        localStorage.setItem('theme','gray')
+        bcBTN.innerHTML = '<i class="fa-solid fa-arrow-down"></i> cinza'
+        document.body.style.backgroundColor = 'gray'
+        document.body.style.color = 'rgb(53, 53, 53)'
+
+
+        direitos.style.color = 'rgb(223, 223, 223)'
+    }
+    if (themevar == 2){
+        localStorage.setItem('theme','white')
+        themevar = 0
+        bcBTN.innerHTML = '<i class="fa-solid fa-arrow-down"></i> branco'
+        document.body.style.backgroundColor = 'white'
+
+        direitos.style.color = 'rgb(135, 135, 135)'
+
+
+    }
+
+})
+
+
+
 btnsearch.addEventListener('click',()=>{
     versediv.innerHTML = ''
     if (book.value == '' || chapter.value == ''){
      alert('por favor,digite o livro e capitulo,ou até mesmo o versiculo, que deseja ler')
     }else if(book.value != '' && chapter.value != '' ){ 
         document.getElementById('errorp').setAttribute('hidden',1)
+
+         let obj = {
+            "book":book.value,
+            "chapter":chapter.value,
+         }
+
+        localStorage.setItem('chapEbook',JSON.stringify(obj))
        
         fetch(`https://bible-api.com/${book.value} ${chapter.value}${verse.value == '' ? '' : ":" + verse.value}?translation=almeida`)
         .then(resp => {
